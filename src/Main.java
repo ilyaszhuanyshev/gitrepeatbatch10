3068536db6a0edff4dde2a67119e0336843d44d3
ilyas <zhuanyshev.ilyas@gmail.com>
2023-12-14 08:13:07 +0600

    Latest One

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String name = "Ilyas";
+        String name = "Ilyas Zhuanyshev";
         int age = 34;
         System.out.println(name + " " + age + " years old");
+
+        System.out.println("New Code");
     }
 }
